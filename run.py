5ce0412b7ef8b137a0427d12fa41f105d41b0e0a
Gabriel Augusto Barbosa <gabriel.a.b@outlook.com>
2015-06-01 23:25:16 -0300

    fixing bluetooth conection in run.py

--- run.py
+++ run.py
@@ -2,24 +2,30 @@ import bluetooth
 import subprocess
 import os
 
-target_name = "OBDII"
+target_name = "XT1058"
 target_address = None
 
 while True:
+
     nearby_devices = bluetooth.discover_devices()
+
     print(nearby_devices)
+
     for device in nearby_devices:
-        if target_name == bluetooth.lookup_name( device ):
+        if target_name == bluetooth.lookup_name(device):
             target_address = device
             break
 
     if target_address is not None:
         print "found target bluetooth device with address ", target_address
         os.system("sudo rfcomm bind all")
+        port = 1
+        sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        sock.connect((target_address, port))
         break
     else:
         print "could not find target bluetooth device nearby"
 
-subprocess.Popen(["python","obd_recorder.py"])
-subprocess.Popen(["python","log/obd_parser.py"])
-subprocess.Popen(["python","obd_gui.py"])
+subprocess.Popen(["python", "obd_recorder.py"])
+subprocess.Popen(["python", "log/obd_parser.py"])
+subprocess.Popen(["python", "obd_gui.py"])
