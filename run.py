72258a459561704839f04419b7d21cf2ace5ae8b
Gabriel Augusto Barbosa <gabriel.a.b@outlook.com>
2015-06-03 23:35:08 -0300

    startx during bluetooth connection

--- run.py
+++ run.py
@@ -1,38 +1,3 @@
-import bluetooth
 import subprocess
-import os
-# import obd
-import time
 
-target_name = "OBDII"
-target_address = None
-
-while True:
-
-    nearby_devices = bluetooth.discover_devices()
-
-    print(nearby_devices)
-
-    for device in nearby_devices:
-        if target_name == bluetooth.lookup_name(device):
-            target_address = device
-            break
-
-    if target_address is not None:
-        print "found target bluetooth device with address ", target_address
-        os.system("sudo rfcomm bind all")
-        time.sleep(5)
-        print "Connected!!!\n\n"
-        """
-        port = 1
-        sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        sock.connect((target_address, port))
-        """
-        break
-    else:
-        print "could not find target bluetooth device nearby"
-
-# connection = obd.OBD()
-subprocess.Popen(["python", "obd_recorder.py"])
-subprocess.Popen(["python", "log/obd_parser.py"])
-subprocess.Popen(["python", "obd_gui.py"])
+subprocess.Popen(["python", "start_all.py"])
